9ba0e77fc831d9f468289a14e3102d0279739fc3
Brendan A R Sechter <bsechter@granicus.if.org>
2016-03-10 00:17:17 +0000

    Added dev.secret.exs.

--- config/dev.exs
+++ config/dev.exs
@@ -32,11 +32,6 @@ config :logger, :console, format: "[$level] $message\n"
 # and calculating stacktraces is usually expensive.
 config :phoenix, :stacktrace_depth, 20
 
-# Configure your database
-config :rumbl, Rumbl.Repo,
-  adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
-  password: "postgres",
-  database: "rumbl_dev",
-  hostname: "localhost",
-  pool_size: 10
+# Finally import the config/dev.secret.exs
+# which should be versioned separately.
+import_config "dev.secret.exs"
