7704b4dcf7ec8d618816ee354ef5db3cf4aeddc3
Brendan A R Sechter <bsechter@sennue.com>
2016-03-12 18:03:28 +0900

    Channel ping.

--- web/static/js/video.js
+++ web/static/js/video.js
@@ -17,6 +17,7 @@ let Video = {
     let postButton = document.getElementById("msg-submit")
     let vidChannel = socket.channel("videos:" + videoId)
 
+    vidChannel.on("ping", ({count}) => console.log("PING", count) )
     vidChannel.join()
       .receive("ok", resp =>
         console.log("joined the video channel", resp)
